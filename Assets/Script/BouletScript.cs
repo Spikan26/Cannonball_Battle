5cdd084980fbf2dbfc6d9b98d3ba629852a3285b
Alexandre <durieuxalexandre26@gmail.com>
2021-09-03 15:48:29 +0200

    Preparation à la 1er itération
    Mise en place des gameObject et des scripts, ainsi que d'un scene simple

---
+++ Assets/Script/BouletScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouletScript : MonoBehaviour
+{
+
+    public int speed = 3;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        Debug.Log("Coucou");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += 1;
+    }
+
+    
+}
